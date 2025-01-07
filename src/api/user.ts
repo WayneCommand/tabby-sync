9989132da8c27a754985712924e6fec88c35dd91
WayneCommand <wayne@waynecommand.com>
2025-01-07 10:09:43 +0800

    Update user.ts

--- src/api/user.ts
+++ src/api/user.ts
@@ -24,7 +24,7 @@ class UserService {
 }
 
 
-// {"id":1,"username":"WayneCommand","active_config":4,"custom_connection_gateway":null,"custom_connection_gateway_token":null,"config_sync_token":"823c2fdf88c85ffac6e0620eb3a5e45f6142782ed574004cf22968ddd9db70c030b63b7c72aa7a0f6f1f407992974784e91811e4e84da805829d7446622ec49a","is_pro":true,"is_sponsor":false,"github_username":"WayneCommand"}
+// {"id":1,"username":"WayneCommand","active_config":4,"custom_connection_gateway":null,"custom_connection_gateway_token":null,"config_sync_token":"1234567890abcde","is_pro":true,"is_sponsor":false,"github_username":"WayneCommand"}
 type User = {
 	id: number,
 	username: string,
